98440f63a5b181e32935c70d819c6b795da82e8b
Justin <theskaterjesus@gmail.com>
2023-06-20 22:03:44 +0100

    Created all migrations

--- database/migrations/2023_06_20_201029_create_travels_table.php
+++ database/migrations/2023_06_20_201029_create_travels_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('travels', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->boolean('is_public')->default(false);
+            $table->text('description');
+            $table->integer('number_of_days');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('travel');
     }
 };
--- database/migrations/2023_06_20_201035_create_tours_table.php
+++ database/migrations/2023_06_20_201035_create_tours_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tours', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->foreignUuid('travel_id')->constrained()->onDelete('cascade');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->timestamp('starting_at');
+            $table->timestamp('ending_at');
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tours');
     }
 };
