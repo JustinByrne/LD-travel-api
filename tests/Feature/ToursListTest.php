49bc76a9c99b442cf0d47c7e26dc21815f2e2034
Justin <theskaterjesus@gmail.com>
2023-06-22 22:28:59 +0100

    Added a list of tests to create for the tours

---
+++ tests/Feature/ToursListTest.php
@@ -0,0 +1,13 @@
+<?php
+
+it('returns a list of tours from a travel based on slug', function () {
+    //
+})->todo();
+
+it('returns a tour with the correct pricing', function () {
+    //
+})->todo();
+
+it('returns tours in a paginated list', function () {
+    //
+})->todo();
