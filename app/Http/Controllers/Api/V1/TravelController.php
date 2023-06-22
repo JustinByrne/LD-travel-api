1e317bd38921ef816ece1eca73b895f06c1c5490
Justin <theskaterjesus@gmail.com>
2023-06-22 22:03:35 +0100

    Added endpoint for all travels

---
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TravelResource;
+use App\Models\Travel;
+
+class TravelController extends Controller
+{
+    public function index()
+    {
+        $travels = Travel::query()
+            ->where('is_public', true)
+            ->paginate();
+
+        return TravelResource::collection($travels);
+    }
+}
