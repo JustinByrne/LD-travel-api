ce846b92bc89c70cf603eec771002789e440dc1b
Justin <theskaterjesus@gmail.com>
2023-06-24 12:58:59 +0100

    Added the tests for the tours endpoint

--- tests/Feature/ToursListTest.php
+++ tests/Feature/ToursListTest.php
@@ -1,13 +1,47 @@
 <?php
 
+use App\Models\Tour;
+use App\Models\Travel;
+use function Pest\Laravel\get;
+
+beforeAll(function () {
+    $this->travel = Travel::factory()->create();
+});
+
 it('returns a list of tours from a travel based on slug', function () {
-    //
-})->todo();
+    $tour = Tour::factory()->create([
+        'travel_id' => $this->travel->id,
+    ]);
+
+    get(route('api.v1.tours', $this->travel))
+        ->assertOk()
+        ->assertJsonCount(1, 'data')
+        ->assertJsonFragment([
+            'id' => $tour->id,
+        ]);
+});
 
 it('returns a tour with the correct pricing', function () {
-    //
-})->todo();
+    Tour::factory()->create([
+        'travel_id' => $this->travel->id,
+        'price' => '123.45',
+    ]);
+
+    get(route('api.v1.tours', $this->travel))
+        ->assertOk()
+        ->assertJsonCount(1, 'data')
+        ->assertJsonFragment([
+            'price' => '123.45',
+        ]);
+});
 
 it('returns tours in a paginated list', function () {
-    //
-})->todo();
+    Tour::factory(16)->create([
+        'travel_id' => $this->travel->id,
+    ]);
+
+    get(route('api.va.tours', $this->travel))
+        ->assertOk()
+        ->assertJsonCount(15, 'data')
+        ->assertJsonPath('meta.last_page', 2);
+});
