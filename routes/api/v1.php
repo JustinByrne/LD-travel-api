bc54c2d9234bf7269dad59740db664e7ab40f8cc
Justin <theskaterjesus@gmail.com>
2023-06-24 13:00:16 +0100

    Added route for getting the tours

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/travels', [TravelController::class, 'index'])->name('travels');
+Route::get('/travels/{travel:slug}/tours', [TourController::class, 'index'])->name('tours');
