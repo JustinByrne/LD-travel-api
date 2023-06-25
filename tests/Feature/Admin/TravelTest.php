b8909bf07566f8fa39d73e948bd4f5e2bb8c42f6
Justin <theskaterjesus@gmail.com>
2023-06-25 15:26:28 +0100

    Added extra tests for editing a travel

--- tests/Feature/Admin/TravelTest.php
+++ tests/Feature/Admin/TravelTest.php
@@ -1,16 +1,22 @@
 <?php
 
 use App\Models\Role;
+use App\Models\Travel;
 use App\Models\User;
 use Database\Seeders\RoleSeeder;
 use function Pest\Faker\fake;
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
 use function Pest\Laravel\postJson;
 
 beforeEach(function () {
     $this->seed(RoleSeeder::class);
 
     $this->user = User::factory()->create();
+    $this->travel = Travel::factory()->create();
+
+    $this->editor_id = Role::where('name', 'editor')->value('id');
+    $this->admin_id = Role::where('name', 'admin')->value('id');
 });
 
 it('returns a 401 error when an unauthenticated user attempt to create a new travel', function () {
@@ -19,7 +25,7 @@ it('returns a 401 error when an unauthenticated user attempt to create a new tra
 });
 
 it('returns a 403 error when an unauthorised user attempt to create a new travel', function () {
-    $this->user->roles()->attach(Role::where('name', 'editor')->value('id'));
+    $this->user->roles()->attach($this->editor_id);
 
     actingAs($this->user)
         ->postJson(route('api.v1.admin.travels.store'))
@@ -27,7 +33,7 @@ it('returns a 403 error when an unauthorised user attempt to create a new travel
 });
 
 it('creates a new travel with valid data', function () {
-    $this->user->roles()->attach(Role::where('name', 'admin')->value('id'));
+    $this->user->roles()->attach($this->admin_id);
 
     $data = [
         'name' => fake()->word(),
@@ -45,10 +51,48 @@ it('creates a new travel with valid data', function () {
 });
 
 it('returns a 422 error when trying to create a new travel with invalid data', function ($data) {
-    $this->user->roles()->attach(Role::where('name', 'admin')->value('id'));
+    $this->user->roles()->attach($this->admin_id);
 
     actingAs($this->user)
         ->postJson(route('api.v1.admin.travels.store'), $data)
+        ->assertUnprocessable();
+})->with([
+    'all required fields are missing' => [
+        [],
+    ],
+    'incorrectly formatted data' => [
+        [
+            'is_public' => 'this is not a boolean',
+            'number_of_days' => 'this is not an integer',
+        ],
+    ],
+]);
+
+it('updates an existing travel with valid data when the user is an editor', function () {
+    $this->user->roles()->attach($this->editor_id);
+
+    $data = [
+        'name' => fake()->word(),
+        'is_public' => fake()->boolean(),
+        'description' => fake()->paragraph(),
+        'number_of_days' => fake()->randomDigitNotNull(),
+    ];
+
+    actingAs($this->user)
+        ->putJson(route('api.v1.admin.travels.update', $this->travel), $data)
+        ->assertOk();
+
+    get(route('api.v1.travels'))
+        ->assertJsonFragment([
+            'name' => $data['name'],
+        ]);
+});
+
+it('returns a 422 error when trying to edit an existing travel with invalid data', function ($data) {
+    $this->user->roles()->attach($this->editor_id);
+
+    actingAs($this->user)
+        ->putJson(route('api.v1.admin.travels.update', $this->travel), $data)
         ->assertUnprocessable();
 })->with([
     'all required fields are missing' => [
