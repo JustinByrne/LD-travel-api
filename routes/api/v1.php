37ec46ac61279bffafd7628db814370bc66807a4
Justin <theskaterjesus@gmail.com>
2023-06-25 14:50:14 +0100

    Added new routes

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Admin\TravelController as AdminTravelController;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/travels', [TravelController::class, 'index'])->name('travels');
 Route::get('/travels/{travel:slug}/tours', [TourController::class, 'index'])->name('tours');
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware([
+        'auth:sanctum',
+        'role:admin',
+    ])
+    ->group(function () {
+        Route::post('travels', [AdminTravelController::class, 'store'])->name('travels.store');
+    });
+
+Route::post('login', LoginController::class)->name('login');
